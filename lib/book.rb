2c0ba871ea0bea8388953b0b64196eb31ad179fc
ximenanavarro <ximenanavarro@Ximenas-MacBook-Pro.local>
2022-12-14 15:22:47 -0700

    commit tree 41aea9a7cecca111748b8316d4834c7422b06272

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,18 @@
+class Book
+    attr_accessor :author, :page_count, :genre
+    attr_reader :title
 
+    def initialize(title, author, page_count, genre)
+        @title = title
+        @author = author
+        @page_count = page_count
+        @genre = genre
+    end
 
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
+
+b = Book.new("Intro to Ruby", "Rawr", 234, "rawr")
+puts b.author
